b648953bfcc92ec85d1bb8f1bd6ecfc70d67945b
Luis Montealegre <montealegreluis@gmail.com>
2016-11-18 15:51:01 -0600

    Extract Sequence object.

---
+++ specs/sequence.spec.js
@@ -0,0 +1,78 @@
+/**
+ * This source file is subject to the license that is bundled with this package in the file LICENSE.
+ */
+"use strict";
+
+describe("Sequence", function () {
+    it("gets the current element", function () {
+        var onlyElement = 3;
+        var sequence = new Sequence([onlyElement]);
+
+        expect(sequence.current()).toBe(onlyElement);
+    });
+
+    it("gets the next element", function () {
+        var secondElement = 3;
+        var sequence = new Sequence([1, secondElement]);
+
+        sequence.next();
+
+        expect(sequence.current()).toBe(secondElement);
+    });
+    it("knows when there are no more elements", function () {
+        var sequence = new Sequence([1, 2]);
+
+        sequence.next();
+        sequence.next();
+        sequence.next();
+
+        expect(sequence.valid()).toBe(false)
+    });
+
+    it("starts iterating again", function () {
+        var firstElement = 1;
+        var sequence = new Sequence([firstElement, 2]);
+
+        sequence.next();
+        sequence.next();
+        sequence.rewind();
+
+        expect(sequence.current()).toBe(firstElement);
+    });
+
+    it("appends more elements to the sequence", function () {
+        var firstNumber = 3;
+        var secondNumber = 2;
+        var sequence = new Sequence();
+
+        sequence.append(firstNumber);
+        sequence.append(secondNumber);
+
+        expect(sequence.current()).toBe(firstNumber);
+        sequence.next();
+        expect(sequence.current()).toBe(secondNumber);
+    });
+
+    it("knows if a partial sequence is correct", function () {
+        var fullSequence = [3, 0, 1];
+        var partialSequence = [3, 0];
+        var sequence = new Sequence(fullSequence);
+
+        expect(sequence.isCorrect(partialSequence)).toBe(true);
+    });
+
+    it("knows if a full sequence is correct", function () {
+        var fullSequence = [3, 0, 1];
+        var sequence = new Sequence(fullSequence);
+
+        expect(sequence.isComplete(fullSequence)).toBe(true);
+    });
+
+    it("clears the current sequence", function () {
+        var sequence = new Sequence();
+
+        sequence.clear();
+
+        expect(sequence.valid()).toBe(false);
+    })
+});
---
+++ src/sequence.js
@@ -0,0 +1,66 @@
+/**
+ * This source file is subject to the license that is bundled with this package in the file LICENSE.
+ */
+(function (window) {
+
+    /**
+     * @param {Array} elements Optional set of elements for this sequence
+     * @constructor
+     */
+    function Sequence(elements) {
+        var sequence = elements || [];
+        var index = 0;
+
+        /**
+         * @returns {Number}
+         */
+        this.current = function () {
+            return sequence[index];
+        };
+
+        this.next = function () {
+            ++index;
+        };
+
+        this.rewind = function () {
+            index = 0;
+        };
+
+        /**
+         * @returns {Boolean}
+         */
+        this.valid = function () {
+            return sequence[index] !== undefined;
+        };
+
+        /**
+         * @param {Number} number
+         */
+        this.append = function (number) {
+            sequence.push(number);
+        };
+
+        /**
+         * @param {Array} playerSequence
+         * @returns {Boolean}
+         */
+        this.isCorrect = function (playerSequence) {
+            return playerSequence.toString() == sequence.slice(0, playerSequence.length).toString();
+        };
+
+        /**
+         * @param {Array} playerSequence
+         * @returns {Boolean}
+         */
+        this.isComplete = function (playerSequence) {
+            return sequence.length === playerSequence.length && this.isCorrect(playerSequence);
+        };
+
+        this.clear = function() {
+            sequence = [];
+            index = 0;
+        };
+    }
+
+    window.Sequence = Sequence;
+})(window);
