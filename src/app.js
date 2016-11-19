043ede1ad2295c35893e686a0a5f7ca7f889bad9
Luis Montealegre <montealegreluis@gmail.com>
2016-11-18 20:11:48 -0600

    Integrate Sequence and SequenceAnimation objects.

--- src/app.js
+++ src/app.js
@@ -3,10 +3,15 @@
  */
 (function ($) {
     var $boxes = $(".well");
-    var board = new Board(new Boxes($boxes));
+    var boxes = new Boxes($boxes);
+    var sequence = new Sequence();
+    var board = new Board(
+        boxes,
+        new SequenceAnimation(sequence, boxes)
+    );
     var game = new Game(
         board,
-        new Simon(board, new RandomGenerator(0, $boxes.length - 1)),
+        new Simon(board, sequence, new RandomGenerator(0, $boxes.length - 1)),
         new Player(board)
     );
     $("#start-game").on("click", game.start);
