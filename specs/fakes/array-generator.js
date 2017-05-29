7b30412e2f8a31a211411bf41527166768c62458
Luis Montealegre <montealegreluis@gmail.com>
2017-05-29 13:02:00 -0500

    Convert the fake ArrayGenerator to an ES6 class

--- specs/fakes/array-generator.js
+++ specs/fakes/array-generator.js
@@ -1,22 +1,23 @@
 /**
  * This source file is subject to the license that is bundled with this package in the file LICENSE.
  */
-(function (window) {
-    function ArrayGenerator(predefinedSequence) {
-        var current = 0;
-        var sequence = predefinedSequence || [];
-
-        this.generate = function() {
-            var currentElement = sequence[current];
-            current++;
-
-            return currentElement;
-        };
-
-        this.changeSequence = function (newSequence) {
-            sequence = newSequence;
-        };
+class ArrayGenerator {
+    /** @param {Array} predefinedSequence */
+    constructor(predefinedSequence = []) {
+        this.current = 0;
+        this.sequence = predefinedSequence;
     }
 
-    window.ArrayGenerator = ArrayGenerator;
-})(window);
+    /** @return {Number} */
+    generate() {
+        const currentElement = this.sequence[this.current];
+        this.current++;
+
+        return currentElement;
+    }
+
+    /** @param {Array} newSequence */
+    changeSequence(newSequence) {
+        this.sequence = newSequence;
+    }
+}
